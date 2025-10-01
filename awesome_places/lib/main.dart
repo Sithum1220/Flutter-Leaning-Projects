37b4c0a27bac325720adb86c40728d9fba9790ee
Sithum Imesh <si@tid.global>
2025-10-01 10:32:50 +0530

    Start Awesome Places app update

--- awesome_places/lib/main.dart
+++ awesome_places/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_places/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,13 +9,9 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {  
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: HomePage()
     );
   }
 }
---
+++ awesome_places/lib/utils/colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+// Primary Colors
+const Color primaryPurple = Color(0xff9E00FF);
+const Color secondaryYellow = Color(0xffFFE500);
+
+// Natural Colors
+const Color black = Color(0xff000000);
+const Color white = Color(0xffFFFFFF);
+
+// Category Colors
+const Color mainNaturalWondersColor = Color(0xff2EB200);
+final Color subNaturalWondersColor = const Color(
+  0xff2EB200,
+).withValues(alpha: 0.53);
+
+const Color mainNightlifeColor = Color(0xffFF9900);
+final Color subNightlifeColor = const Color(0xffFF9900).withValues(alpha: 0.53);
+
+const Color mainLandmarksColor = Color(0xff0029FF);
+final Color subLandmarksColor = const Color(0xff0029FF).withValues(alpha: 0.53);
+
+const Color mainCulturalColor = Color(0xffC00C00);
+final Color subCulturalColor = const Color(0xffC00C00).withValues(alpha: 0.53);
+
+// Category Button Colors
+const Color darkCategoryButtonColor = Color(0xff8E8FFA);
+const Color lightCategoryButtonColor = Color(0xffC2D9FF);
+
+// Text Colors
+final Color mainTextColor = const Color(0xff3B3636).withValues(alpha: 0.75);
+
+// Card Background Colors
+final Color cardBgColor = const Color(0xffCACACA).withValues(alpha: 0.31);
+
+// Add And Remove Button Colors
+const Color addButtonColor = Color(0xff06FFA5);
+const Color removeButtonColor = Color(0xffFF1E0F);
