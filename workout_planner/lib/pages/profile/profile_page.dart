1b961dfd306e053e81d2c77d8bd501166b2f3400
Sithum Imesh <si@tid.global>
2025-10-10 19:34:32 +0530

    work planner app update

--- workout_planner/lib/pages/profile/profile_page.dart
+++ workout_planner/lib/pages/profile/profile_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:workout_planner/constants/colors.dart';
+import 'package:workout_planner/constants/responsive.dart';
+import 'package:workout_planner/data/user_data.dart';
+import 'package:workout_planner/models/user_model.dart';
+import 'package:workout_planner/widgets/progress_card.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,13 +14,53 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final DateFormat dateFormatter = DateFormat('EEEE , MMMM');
+  final DateFormat dayFormatter = DateFormat('dd');
+  final UserModel userData = user;
+
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = dateFormatter.format(now);
+    String formattedDay = dayFormatter.format(now);
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Center(
-            child: Text("Profile  Page"),
+          child: Padding(
+            padding: const EdgeInsets.all(kDefualtPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$formattedDate $formattedDay",
+                  style: const TextStyle(
+                    fontSize: 15,
+                    color: kSubTitleColor,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  userData.fullName,
+                  style: const TextStyle(
+                    fontSize: 40,
+                    color: kMainBlackColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Your Progress",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: kMainBlackColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                ProgressCard(progressValue: 0.4, total: 100),
+              ],
+            ),
           ),
         ),
       ),
